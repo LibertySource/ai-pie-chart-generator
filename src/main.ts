fbccb2bd37f12d3fc82d0aa5b6cd8be017b29730
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2024-11-24 23:43:42 -0600

    fix: typescript appVersion error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,4 @@ import App from './App.vue'
 
 const app = createApp(App);
 
-// Add the version to global properties
-app.config.globalProperties.$appVersion = __APP_VERSION__;
-
 app.mount('#app');
